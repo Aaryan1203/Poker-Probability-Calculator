4e51e70c013776396eeef3b660ef8094b084a503
aaryan1203 <aaryanjain1203@gmail.com>
2023-08-08 15:42:52 -0400

    Add clickable players

--- src/App.js
+++ src/App.js
@@ -33,7 +33,11 @@ function Card({ className, onClick, cardDetails }) {
   }
 
   return (
-    <div className={`card ${className}`} onClick={onClick}>
+    <div
+      className={`card ${className}`}
+      onClick={onClick}
+      style={{ backgroundColor: "white" }}
+    >
       {cardDetails && (
         <>
           <span style={{ paddingLeft: "4px" }}>{cardDetails.cardName}</span>
@@ -52,6 +56,9 @@ function DeckCard({
   setSelectedCard,
   usedDeckCards,
   setUsedDeckCards,
+  communityCards,
+  setPlayerCards,
+  playerCards,
 }) {
   let suitImage;
 
@@ -80,22 +87,33 @@ function DeckCard({
           : ""
       } ${usedDeckCards.has(cardNumber) ? "used-deck-card" : ""}`}
       onClick={() => {
-        setSelectedCard({
-          type: "deck",
-          cardNumber: cardNumber,
-          cardName: cardName,
-          cardSuit: cardSuit,
-          cardDetails: {
+        if (selectedCard.type === "player") {
+          const newPlayerCards = [...playerCards];
+          newPlayerCards[selectedCard.playerIndex][selectedCard.cardIndex] = {
+            cardNumber: cardNumber,
             cardName: cardName,
             cardSuit: cardSuit,
+          };
+          setSelectedCard({});
+          setUsedDeckCards((prevUsedDeckCards) => {
+            const newUsedDeckCards = new Set(prevUsedDeckCards);
+            newUsedDeckCards.add(cardNumber);
+            return newUsedDeckCards;
+          });
+        } else if (selectedCard.type === "community") {
+          const newCommunityCards = [...communityCards];
+          newCommunityCards[selectedCard.cardIndex] = {
             cardNumber: cardNumber,
-          },
-        });
-        setUsedDeckCards((prevUsedDeckCards) => {
-          const newUsedDeckCards = new Set(prevUsedDeckCards);
-          newUsedDeckCards.add(cardNumber);
-          return newUsedDeckCards;
-        });
+            cardName: cardName,
+            cardSuit: cardSuit,
+          };
+          setSelectedCard({});
+          setUsedDeckCards((prevUsedDeckCards) => {
+            const newUsedDeckCards = new Set(prevUsedDeckCards);
+            newUsedDeckCards.add(cardNumber);
+            return newUsedDeckCards;
+          });
+        }
       }}
     >
       <span style={{ paddingLeft: "4px" }}>{cardName}</span>
@@ -109,6 +127,9 @@ function Deck({
   setSelectedCard,
   usedDeckCards,
   setUsedDeckCards,
+  communityCards,
+  playerCards,
+  setPlayerCards,
 }) {
   const cardSuits = ["spade", "heart", "club", "diamond"];
   const cardRanks = [
@@ -143,6 +164,9 @@ function Deck({
               setSelectedCard={setSelectedCard}
               usedDeckCards={usedDeckCards}
               setUsedDeckCards={setUsedDeckCards}
+              communityCards={communityCards}
+              playerCards={playerCards} // pass the playerCards to DeckCard
+              setPlayerCards={setPlayerCards} // pass the setPlayerCards to DeckCard
             />
           );
         })}
@@ -159,9 +183,10 @@ function Player({
   numPlayers,
   selectedCard,
   setSelectedCard,
+  playerCards,
 }) {
   const [isActive, setIsActive] = useState(playerNumber <= 2);
-  const cards = new Array(numCards).fill(null);
+  const cards = playerCards[playerNumber - 1];
   const cardOpacity = isActive ? 1 : 0.5;
   const addPlayerButton = isActive ? "-" : "+";
   const activeCardClass = isActive ? "active-card" : "non-active-card";
@@ -212,7 +237,7 @@ function Player({
                 }));
               }
             }}
-            cardDetails={selectedCard.cardDetails}
+            cardDetails={cards[index]}
           />
         ))}
       </div>
@@ -224,9 +249,9 @@ function CommunityCard({
   index,
   selectedCard,
   setSelectedCard,
-  setCommunityCards,
+  communityCards,
 }) {
-  if (!selectedCard || !selectedCard.cardDetails) {
+  if (!communityCards[index]) {
     return (
       <div
         className={`community-card ${
@@ -238,34 +263,27 @@ function CommunityCard({
     );
   }
 
-  const cardDetails = selectedCard?.cardDetails;
+  const cardDetails = communityCards[index];
   const suitImage =
     cardDetails && cardDetails.cardSuit ? getImage(cardDetails.cardSuit) : "";
 
   return (
     <div
       className={`community-card ${
-        selectedCard?.type === "community" && selectedCard?.index === index
+        selectedCard.type === "community" && selectedCard?.cardIndex === index
           ? "selected-card"
           : ""
       }`}
       onClick={() => {
-        if (cardDetails) {
-          setSelectedCard((prevSelectedCard) => ({
-            type: "community",
-            index: index,
-            cardDetails: prevSelectedCard?.cardDetails,
-          }));
-          setCommunityCards((prevCommunityCards) => {
-            const newCommunityCards = [...prevCommunityCards];
-            newCommunityCards[index] = cardDetails;
-            return newCommunityCards;
-          });
-        }
+        setSelectedCard({
+          type: "community",
+          cardIndex: index,
+          cardDetails: cardDetails,
+        });
       }}
     >
       <span style={{ paddingLeft: "6px" }}>
-        {selectedCard.cardDetails.cardName}
+        {cardDetails ? cardDetails.cardName : ""}
       </span>
       <img src={suitImage} alt="suit" className="community-suit-image" />
     </div>
@@ -304,47 +322,57 @@ function PokerGame({ numCards, numPlayers }) {
   });
   const [usedDeckCards, setUsedDeckCards] = useState(new Set());
   const [communityCards, setCommunityCards] = useState(Array(5).fill(null));
+  const [playerCards, setPlayerCards] = useState(
+    Array.from({ length: numPlayers }, () => Array(numCards).fill(null))
+  );
 
   const reset = () => {
     setSelectedCard({
       type: null,
       playerIndex: null,
       cardIndex: null,
+      cardDetails: null,
     });
     setUsedDeckCards(new Set());
+    setPlayerCards(
+      Array.from({ length: numPlayers }, () => Array(numCards).fill(null))
+    );
   };
 
-const communityCardElements = [
-  <div key={0} className="community-cards-group">
-    {communityCards.slice(0, 3).map((_, index) => (
+  const communityCardElements = [
+    <div key={0} className="community-cards-group">
+      {communityCards.slice(0, 3).map((_, index) => (
+        <CommunityCard
+          key={index}
+          index={index}
+          selectedCard={selectedCard}
+          setSelectedCard={setSelectedCard}
+          setCommunityCards={setCommunityCards}
+          communityCards={communityCards}
+        />
+      ))}
+    </div>,
+    <div key={1} className="community-cards-group">
       <CommunityCard
-        key={index}
-        index={index}
+        key={1}
+        index={3}
         selectedCard={selectedCard}
         setSelectedCard={setSelectedCard}
         setCommunityCards={setCommunityCards}
+        communityCards={communityCards}
       />
-    ))}
-  </div>,
-  <div key={1} className="community-cards-group">
-    <CommunityCard
-      key={1}
-      index={3}
-      selectedCard={selectedCard}
-      setSelectedCard={setSelectedCard}
-      setCommunityCards={setCommunityCards}
-    />
-  </div>,
-  <div key={2} className="community-cards-group">
-    <CommunityCard
-      key={2}
-      index={4}
-      selectedCard={selectedCard}
-      setSelectedCard={setSelectedCard}
-      setCommunityCards={setCommunityCards}
-    />
-  </div>,
-];
+    </div>,
+    <div key={2} className="community-cards-group">
+      <CommunityCard
+        key={2}
+        index={4}
+        selectedCard={selectedCard}
+        setSelectedCard={setSelectedCard}
+        setCommunityCards={setCommunityCards}
+        communityCards={communityCards}
+      />
+    </div>,
+  ];
 
   const players = new Array(numPlayers)
     .fill(null)
@@ -356,6 +384,7 @@ const communityCardElements = [
         numPlayers={numPlayers}
         selectedCard={selectedCard}
         setSelectedCard={setSelectedCard}
+        playerCards={playerCards}
       />
     ));
 
@@ -375,6 +404,9 @@ const communityCardElements = [
           setSelectedCard={setSelectedCard}
           usedDeckCards={usedDeckCards}
           setUsedDeckCards={setUsedDeckCards}
+          communityCards={communityCards}
+          playerCards={playerCards}
+          setPlayerCards={setPlayerCards} // pass the setPlayerCards to Deck
         />
       </div>
     </div>
